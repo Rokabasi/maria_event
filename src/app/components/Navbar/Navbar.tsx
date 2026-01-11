0e14a36d804e1e980e4d068fe50eb2c4c4687a9a
Lionel Mutombo <lionelmutombo.mtb@gmail.com>
2026-01-11 17:56:03 +0100

    feat: add cart

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -17,6 +17,29 @@ export default function Navbar() {
       <div className="flex items-center justify-between p-4 relative z-50">
         <Menu isActive={isMenuOpen} onClick={handleMenuClick} />
         <Logo />
+        <div className="relative">
+          <div className="w-10 h-10 border border-gray-300 rounded-full flex items-center justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-shopping-cart-icon lucide-shopping-cart"
+            >
+              <circle cx="8" cy="21" r="1" />
+              <circle cx="19" cy="21" r="1" />
+              <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" />
+            </svg>
+          </div>
+          <span className="absolute bg-black text-white text-xs rounded-full w-4 h-4 -right-1 -bottom-1 flex items-center justify-center">
+            0
+          </span>
+        </div>
       </div>
 
       {/* Menu mobile overlay */}
